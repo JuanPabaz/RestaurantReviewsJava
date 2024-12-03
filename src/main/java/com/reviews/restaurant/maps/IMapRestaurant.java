35f9057fd8225a0104479e70c09e89b9d8c49455
JuanPabaz <jpgthf@gmail.com>
2024-12-03 15:14:36 -0500

    feat: add IMapRestaurant

---
+++ src/main/java/com/reviews/restaurant/maps/IMapRestaurant.java
@@ -0,0 +1,25 @@
+package com.reviews.restaurant.maps;
+
+import com.reviews.restaurant.dto.RestaurantDTO;
+import com.reviews.restaurant.entities.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IMapRestaurant {
+
+    @Mappings({
+            @Mapping(source = "idRestaurant",target = "idRestaurant"),
+            @Mapping(source = "restuarantName", target = "restuarantName"),
+            @Mapping(source = "address", target = "address"),
+            @Mapping(source = "phoneNumber", target = "phoneNumber"),
+            @Mapping(source = "category", target = "category.idCategoria")
+    })
+    RestaurantDTO mapRestaurant(Restaurant restaurant);
+
+    List<RestaurantDTO> mapRestaurantList(List<Restaurant> restaurantList);
+
+}
