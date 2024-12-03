dc306fd53679db25c6ccf723d6079a73c53bc0df
JuanPabaz <jpgthf@gmail.com>
2024-12-03 17:19:18 -0500

    feat: add IMapImage

---
+++ src/main/java/com/reviews/restaurant/maps/IMapImage.java
@@ -0,0 +1,26 @@
+package com.reviews.restaurant.maps;
+
+import com.reviews.restaurant.dto.ImageDTO;
+import com.reviews.restaurant.dto.RestaurantDTO;
+import com.reviews.restaurant.entities.Image;
+import com.reviews.restaurant.entities.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IMapImage{
+
+    @Mappings({
+            @Mapping(source = "idImage",target = "idImage"),
+            @Mapping(source = "image", target = "image"),
+            @Mapping(source = "restaurant.id", target = "restaurant"),
+            @Mapping(source = "review", target = "review"),
+            @Mapping(source = "category", target = "category.idCategoria")
+    })
+    ImageDTO mapImage(Image image);
+
+    List<ImageDTO> mapImageList(List<Image> imageList);
+}
