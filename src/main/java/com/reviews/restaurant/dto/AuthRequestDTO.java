80b94db531c188b530f29653126463e98bba2d2f
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:32:39 -0500

    feat: create AuthRequestDTO

---
+++ src/main/java/com/reviews/restaurant/dto/AuthRequestDTO.java
@@ -0,0 +1,14 @@
+package com.reviews.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequestDTO {
+
+    private String username;
+    private String password;
+}
