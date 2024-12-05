b0b6e31eeabe7db5aaf43741ce5f87e8efddcbb2
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:42:38 -0500

    feat: change attribute names to english

--- src/main/java/com/reviews/restaurant/dto/RestaurantDTO.java
+++ src/main/java/com/reviews/restaurant/dto/RestaurantDTO.java
@@ -1,19 +1,20 @@
 package com.reviews.restaurant.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Data
+@NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class RestaurantDTO {
 
-    private Long idRestaurante;
-    private String nombreRestaurante;
-    private String direccion;
-    private String telefono;
-    private Long idCategoria;
-    private List<Long> imagenList;
-    private List<Long> resenaList;
+    private Long idRestaurant;
+    private String restuarantName;
+    private String address;
+    private String phoneNumber;
+    private Long idCategory;
 }
