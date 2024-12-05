1b7b0b5232710c6860a356d9714f637d0e0aa145
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:42:03 -0500

    feat: create RefreshTokenService

---
+++ src/main/java/com/reviews/restaurant/service/RefreshTokenService.java
@@ -0,0 +1,61 @@
+package com.reviews.restaurant.service;
+
+import com.reviews.restaurant.entities.RefreshToken;
+import com.reviews.restaurant.exceptions.ExpiredRefreshTokenException;
+import com.reviews.restaurant.repositories.RefreshTokenRepository;
+import com.reviews.restaurant.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Value("${application.security.jwt.refresh-token-expiration}")
+    private long refreshTokenExpire;
+
+    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository, UsuarioRepository usuarioRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public RefreshToken createRefreshToken(String username){
+        RefreshToken refreshToken = RefreshToken.builder()
+                .usuario(usuarioRepository.findByUsername(username).get())
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(refreshTokenExpire))
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+
+
+    public Optional<RefreshToken> findByToken(String token){
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token){
+        if(token.getExpiryDate().compareTo(Instant.now())<0){
+            refreshTokenRepository.delete(token);
+            throw new ExpiredRefreshTokenException("El token de actualización " + token.getToken() +  " esta vencido. Inicia sesión de nuevo");
+        }
+        return token;
+    }
+
+
+
+    public Optional<RefreshToken> findByUsername(String username){
+        return refreshTokenRepository.findByUsername(username);
+    }
+
+    public void DeleteRefreshToken(RefreshToken refreshToken){
+        refreshTokenRepository.delete(refreshToken);
+    }
+}
