ae47cf696cb17927d7b7b4c6e4809c3749997b03
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:32:48 -0500

    feat: create AuthResponseDTO

---
+++ src/main/java/com/reviews/restaurant/dto/AuthResponseDTO.java
@@ -0,0 +1,19 @@
+package com.reviews.restaurant.dto;
+
+import com.reviews.restaurant.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponseDTO {
+
+    private String accessToken;
+    private String refreshToken;
+    private Role role;
+
+}
