c4228e136e44ee11070bdcdaf0fc39fcf9a35440
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:33:53 -0500

    feat: add the @AllArgsConstructor and @NoArgsConstructor annotation and change idImage type

--- src/main/java/com/reviews/restaurant/dto/CategoryDTO.java
+++ src/main/java/com/reviews/restaurant/dto/CategoryDTO.java
@@ -1,15 +1,18 @@
 package com.reviews.restaurant.dto;
 
 import com.reviews.restaurant.entities.Image;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
 
     private Long idCategory;
     private String category;
     private String description;
-    private Image imageCategory;
+    private Long idImage;
 }
