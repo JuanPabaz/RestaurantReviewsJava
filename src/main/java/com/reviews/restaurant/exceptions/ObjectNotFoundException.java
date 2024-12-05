94c4362a05d9bc069f00ecd3d6931d3ad79eb26a
JuanPabaz <jpgthf@gmail.com>
2024-12-05 10:36:28 -0500

    fix: create ObjectNotFoundException

---
+++ src/main/java/com/reviews/restaurant/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.reviews.restaurant.exceptions;
+
+public class ObjectNotFoundException extends Exception{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
