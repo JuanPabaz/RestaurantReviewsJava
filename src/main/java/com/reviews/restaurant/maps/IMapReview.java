66860c3ba6dfc033f784cad78d8d23f01497b60d
JuanPabaz <jpgthf@gmail.com>
2024-12-06 17:50:29 -0500

    feat: create IMapReview

---
+++ src/main/java/com/reviews/restaurant/maps/IMapReview.java
@@ -0,0 +1,30 @@
+package com.reviews.restaurant.maps;
+
+import com.reviews.restaurant.dto.ReviewResponseDTO;
+import com.reviews.restaurant.entities.Review;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IMapReview {
+
+    @Mappings({
+            @Mapping(source = "idReview", target = "idReview"),
+            @Mapping(source = "place",target = "place"),
+            @Mapping(source = "food", target = "food"),
+            @Mapping(source = "service", target = "service"),
+            @Mapping(source = "drinks", target = "drinks"),
+            @Mapping(source = "music", target = "music"),
+            @Mapping(source = "menu", target = "menu"),
+            @Mapping(source = "waitingTime", target = "waitingTime"),
+            @Mapping(source = "comments", target = "comments"),
+            @Mapping(source = "Restaurant.idRestaurant", target = "idRestaurant"),
+            @Mapping(source = "User.idUser", target = "idUser"),
+    })
+    ReviewResponseDTO mapReview(Review review);
+
+    List<ReviewResponseDTO> mapReviewList(List<Review> reviewList);
+}
