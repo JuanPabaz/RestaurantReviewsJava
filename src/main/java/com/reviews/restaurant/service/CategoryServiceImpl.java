29133c4dce77b83d4c63f26d295d16282643df54
JuanPabaz <jpgthf@gmail.com>
2024-12-06 19:08:59 -0500

    feat: add Service Annotation

--- src/main/java/com/reviews/restaurant/service/CategoryServiceImpl.java
+++ src/main/java/com/reviews/restaurant/service/CategoryServiceImpl.java
@@ -3,9 +3,11 @@ package com.reviews.restaurant.service;
 import com.reviews.restaurant.dto.CategoryDTO;
 import com.reviews.restaurant.maps.IMapCategory;
 import com.reviews.restaurant.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements ICategoryService {
 
     private final CategoryRepository categoryRepository;
