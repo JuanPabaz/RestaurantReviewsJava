58e32d7ec841643965cfaca619e0669052433263
JuanPabaz <jpgthf@gmail.com>
2024-12-06 19:27:31 -0500

    feat: add method addRestaurant

--- src/main/java/com/reviews/restaurant/service/IRestaurantService.java
+++ src/main/java/com/reviews/restaurant/service/IRestaurantService.java
@@ -1,10 +1,14 @@
 package com.reviews.restaurant.service;
 
-import com.reviews.restaurant.dto.RestaurantDTO;
+import com.reviews.restaurant.dto.RestaurantRequestDTO;
+import com.reviews.restaurant.dto.RestaurantResponseDTO;
+import org.apache.coyote.BadRequestException;
 
 import java.util.List;
 
 public interface IRestaurantService {
 
-    List<RestaurantDTO> getAllRestaurants();
+    List<RestaurantResponseDTO> getAllRestaurants();
+
+    RestaurantResponseDTO addRestaurant(RestaurantRequestDTO restaurant) throws BadRequestException;
 }
