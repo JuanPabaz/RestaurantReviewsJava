4eaf02235e426a88812ad4fe4c855b2fdc9041bb
JuanPabaz <jpgthf@gmail.com>
2024-12-06 19:27:13 -0500

    feat: add RestaurantRequestDTO

---
+++ src/main/java/com/reviews/restaurant/dto/RestaurantRequestDTO.java
@@ -0,0 +1,18 @@
+package com.reviews.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class RestaurantRequestDTO {
+
+    private String restuarantName;
+    private String address;
+    private String phoneNumber;
+    private Long idCategory;
+}
