e521c07f0fcd3f9e96dd781b84b605afb90c4bad
JuanPabaz <jpgthf@gmail.com>
2024-12-06 19:27:26 -0500

    feat: add method addRestaurant

--- src/main/java/com/reviews/restaurant/service/RestaurantServiceImpl.java
+++ src/main/java/com/reviews/restaurant/service/RestaurantServiceImpl.java
@@ -1,8 +1,14 @@
 package com.reviews.restaurant.service;
 
-import com.reviews.restaurant.dto.RestaurantDTO;
+import com.reviews.restaurant.dto.RestaurantRequestDTO;
+import com.reviews.restaurant.dto.RestaurantResponseDTO;
+import com.reviews.restaurant.entities.Category;
+import com.reviews.restaurant.entities.Restaurant;
+import com.reviews.restaurant.exceptions.BadCreateRequest;
 import com.reviews.restaurant.maps.IMapRestaurant;
+import com.reviews.restaurant.repositories.CategoryRepository;
 import com.reviews.restaurant.repositories.RestaurantRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +20,41 @@ public class RestaurantServiceImpl implements IRestaurantService {
 
     private final IMapRestaurant mapRestaurant;
 
-    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, IMapRestaurant mapRestaurant) {
+    private final CategoryRepository categoryRepository;
+
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, IMapRestaurant mapRestaurant, CategoryRepository categoryRepository) {
         this.restaurantRepository = restaurantRepository;
         this.mapRestaurant = mapRestaurant;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public List<RestaurantDTO> getAllRestaurants() {
+    public List<RestaurantResponseDTO> getAllRestaurants() {
         return mapRestaurant.mapRestaurantList(restaurantRepository.findAll());
+    }
+
+    @Override
+    public RestaurantResponseDTO addRestaurant(RestaurantRequestDTO restaurantRequestDTO) throws BadRequestException {
+        Category category = categoryRepository.findById(restaurantRequestDTO.getIdCategory()).orElseThrow(() -> new BadCreateRequest("La categoria no existe"));
+        if (restaurantRequestDTO.getRestuarantName().isEmpty()) {
+            throw new BadRequestException("El nombre del restaurante no puede estar vacio");
+        }
+        if (restaurantRequestDTO.getPhoneNumber().isEmpty()) {
+            throw new BadRequestException("El numero de telefono no puede estar vacio");
+        }
+        if (restaurantRequestDTO.getAddress().isEmpty()) {
+            throw new BadRequestException("La direccion del restaurante no puede estar vacia");
+        }
+        Restaurant restaurant = convertToEntity(restaurantRequestDTO, category);
+        return mapRestaurant.mapRestaurant(restaurantRepository.save(restaurant));
+    }
+
+    private Restaurant convertToEntity(RestaurantRequestDTO restaurantRequestDTO, Category category) {
+        return Restaurant.builder()
+                .address(restaurantRequestDTO.getAddress())
+                .phoneNumber(restaurantRequestDTO.getPhoneNumber())
+                .category(category)
+                .restuarantName(restaurantRequestDTO.getRestuarantName())
+                .build();
     }
 }
