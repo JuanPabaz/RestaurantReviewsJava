b71dad6d196fca833ece7e88636a7eb0f8a30daa
JuanPabaz <jpgthf@gmail.com>
2024-12-10 15:03:43 -0500

    feat: add findImagesByRestaurant

--- src/main/java/com/reviews/restaurant/repositories/ImageRepository.java
+++ src/main/java/com/reviews/restaurant/repositories/ImageRepository.java
@@ -1,7 +1,15 @@
 package com.reviews.restaurant.repositories;
 
+import com.reviews.restaurant.dto.ImageResponseDTO;
 import com.reviews.restaurant.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query("SELECT m FROM Image AS m WHERE m.restaurant.idRestaurant = :idRestaurant")
+    List<Image> findImagesByRestaurant(@Param("idRestaurant") Long idRestaurant);
 }
