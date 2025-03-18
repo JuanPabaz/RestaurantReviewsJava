e1a1b05415afda36067646f39df2c2f180decf36
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-17 19:40:45 -0500

    feat: add bucketUrl

--- src/main/java/com/reviews/restaurant/utils/Constants.java
+++ src/main/java/com/reviews/restaurant/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
     public static final String RESTAURANT_BUCKET = "restaurant-review-images-202400";
     public static final String RESTAURANT_BUCKET_FOLDER = "restaurant-images/";
+    public static final String BUCKET_URL = "https://restaurant-review-images-202400.s3.us-east-1.amazonaws.com/";
 }
