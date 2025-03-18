753fad93e2eb0f81ce01996b92511d8b198dd2dc
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-17 19:42:15 -0500

    feat: change method parameters

--- src/main/java/com/reviews/restaurant/service/IReviewService.java
+++ src/main/java/com/reviews/restaurant/service/IReviewService.java
@@ -5,10 +5,13 @@ import com.reviews.restaurant.dto.ReviewResponseDTO;
 import com.reviews.restaurant.entities.Restaurant;
 import com.reviews.restaurant.entities.Review;
 import com.reviews.restaurant.entities.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IReviewService {
 
-    ReviewResponseDTO addReview(ReviewRequestDTO review);
+    ReviewResponseDTO addReview(ReviewRequestDTO review, List<MultipartFile> images);
 
     Review convertToEntity(ReviewRequestDTO review, Restaurant restaurant, User user);
 }
