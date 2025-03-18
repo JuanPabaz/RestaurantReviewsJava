7dfe875ece2b9fc5be7ecc926de868ced41fdcfe
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-17 19:41:52 -0500

    feat: change endpoint so it receives a form-data

--- src/main/java/com/reviews/restaurant/controllers/ReviewController.java
+++ src/main/java/com/reviews/restaurant/controllers/ReviewController.java
@@ -1,9 +1,15 @@
 package com.reviews.restaurant.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reviews.restaurant.dto.ReviewRequestDTO;
 import com.reviews.restaurant.dto.ReviewResponseDTO;
 import com.reviews.restaurant.service.IReviewService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/review")
@@ -16,8 +22,14 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @PostMapping()
-    public ReviewResponseDTO addReview(@RequestBody ReviewRequestDTO reviewRequestDTO) {
-        return reviewService.addReview(reviewRequestDTO);
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ReviewResponseDTO addReview(
+            @RequestPart("review") String reviewJson,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images
+    ) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ReviewRequestDTO reviewRequestDTO = objectMapper.readValue(reviewJson, ReviewRequestDTO.class);
+        return reviewService.addReview(reviewRequestDTO, images);
     }
+
 }
