0ff515ab0dfa377b715bd1e24bf1079c81cfc675
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-17 19:37:43 -0500

    feat: implement new uploadFile overload version

--- src/main/java/com/reviews/restaurant/service/S3ServiceImpl.java
+++ src/main/java/com/reviews/restaurant/service/S3ServiceImpl.java
@@ -1,9 +1,12 @@
 package com.reviews.restaurant.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
@@ -12,6 +15,9 @@ import java.util.List;
 public class S3ServiceImpl implements IS3Service {
 
     private final S3Client s3Client;
+
+    @Value("${spring.destination.folder}")
+    private String destinationFolder;
 
     public S3ServiceImpl(S3Client s3Client) {
         this.s3Client = s3Client;
@@ -44,6 +50,18 @@ public class S3ServiceImpl implements IS3Service {
                 .build();
         PutObjectResponse putObjectResponse = this.s3Client.putObject(putObjectRequest,fileLocation);
         return putObjectResponse.sdkHttpResponse().isSuccessful();
+    }
+
+    @Override
+    public Boolean uploadFile(String bucketName, String key, MultipartFile file) {
+        try {
+            Path tempFile = Files.createTempFile("upload-", file.getOriginalFilename());
+            file.transferTo(tempFile.toFile());
+            return uploadFile(bucketName, key, tempFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
