e85d48987a3be5da9c47ac2b2f9883d437048740
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-18 14:50:38 -0500

    feat: add saveImages logic to s3 service

--- src/main/java/com/reviews/restaurant/service/S3ServiceImpl.java
+++ src/main/java/com/reviews/restaurant/service/S3ServiceImpl.java
@@ -1,15 +1,23 @@
 package com.reviews.restaurant.service;
 
+import com.reviews.restaurant.entities.Image;
+import com.reviews.restaurant.entities.Restaurant;
+import com.reviews.restaurant.entities.Review;
+import com.reviews.restaurant.exceptions.BadCreateRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.reviews.restaurant.utils.Constants.*;
 
 @Service
 public class S3ServiceImpl implements IS3Service {
@@ -72,5 +80,31 @@ public class S3ServiceImpl implements IS3Service {
     @Override
     public String generatePresignedDownloadloadUrl(String bucketName, String key, Duration duration) {
         return "";
+    }
+
+    public List<Image> saveImages(List<MultipartFile> images, Review review, Restaurant restaurant) {
+        List<Image> imageResponse = new ArrayList<>();
+        try{
+            if (images != null) {
+                for (MultipartFile image : images) {
+                    String key = RESTAURANT_BUCKET_FOLDER + image.getOriginalFilename();
+                    Path tempFile = Files.createTempFile("upload-", image.getOriginalFilename());
+                    image.transferTo(tempFile.toFile());
+
+                    boolean uploadSuccess = uploadFile(RESTAURANT_BUCKET, key, tempFile);
+                    if (uploadSuccess) {
+                        Image imageEntity = new Image();
+                        imageEntity.setImage(BUCKET_URL + key);
+                        imageEntity.setReview(review);
+                        imageEntity.setRestaurant(restaurant);
+                        imageResponse.add(imageEntity);
+                    }
+                }
+            }
+        }catch (IOException e){
+            throw new BadCreateRequest("Error al cargar imagen");
+        }
+
+        return imageResponse;
     }
 }
