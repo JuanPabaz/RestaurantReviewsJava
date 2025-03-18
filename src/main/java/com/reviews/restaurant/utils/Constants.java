fecd2075296c7d00f4013d24b257fcbc159d94a9
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-17 19:35:38 -0500

    feat: create Constants class for bucket related names

---
+++ src/main/java/com/reviews/restaurant/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.reviews.restaurant.utils;
+
+public class Constants {
+
+    public static final String RESTAURANT_BUCKET = "restaurant-review-images-202400";
+    public static final String RESTAURANT_BUCKET_FOLDER = "restaurant-images/";
+}
