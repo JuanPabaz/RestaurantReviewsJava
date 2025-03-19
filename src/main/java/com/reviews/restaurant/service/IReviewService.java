e5064efed7e9e3f79cbf39082c3a6abd690375b5
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-19 12:13:33 -0500

    feat: add listReviews method to interface

--- src/main/java/com/reviews/restaurant/service/IReviewService.java
+++ src/main/java/com/reviews/restaurant/service/IReviewService.java
@@ -5,6 +5,8 @@ import com.reviews.restaurant.dto.ReviewResponseDTO;
 import com.reviews.restaurant.entities.Restaurant;
 import com.reviews.restaurant.entities.Review;
 import com.reviews.restaurant.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -14,4 +16,6 @@ public interface IReviewService {
     ReviewResponseDTO addReview(ReviewRequestDTO review, List<MultipartFile> images);
 
     Review convertToEntity(ReviewRequestDTO review, Restaurant restaurant, User user);
+
+    Page<ReviewResponseDTO> listReviews(Pageable pageable);
 }
