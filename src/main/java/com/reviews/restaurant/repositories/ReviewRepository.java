7bf20899274fba3d38865a1c2f6b931f8c940d37
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 14:44:02 -0500

    feat: add findReviewByRestaurantName query

--- src/main/java/com/reviews/restaurant/repositories/ReviewRepository.java
+++ src/main/java/com/reviews/restaurant/repositories/ReviewRepository.java
@@ -1,7 +1,16 @@
 package com.reviews.restaurant.repositories;
 
 import com.reviews.restaurant.entities.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT r FROM Review AS r INNER JOIN Restaurant AS res " +
+            "ON r.restaurant.idRestaurant = res.idRestaurant " +
+            "WHERE res.restuarantName LIKE %:restaurantName%")
+    Page<Review> findByRestaurantName(@Param("restaurantName") String restaurantName, Pageable pageable);
 }
