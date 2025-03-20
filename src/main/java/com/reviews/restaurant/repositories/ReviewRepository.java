7acfe9562dcafcab7b0e544d6cbec821325c875b
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 15:58:43 -0500

    feat: use ILIKE to ignore casing

--- src/main/java/com/reviews/restaurant/repositories/ReviewRepository.java
+++ src/main/java/com/reviews/restaurant/repositories/ReviewRepository.java
@@ -11,6 +11,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT r FROM Review AS r INNER JOIN Restaurant AS res " +
             "ON r.restaurant.idRestaurant = res.idRestaurant " +
-            "WHERE res.restuarantName LIKE %:restaurantName%")
+            "WHERE res.restuarantName ILIKE %:restaurantName%")
     Page<Review> findByRestaurantName(@Param("restaurantName") String restaurantName, Pageable pageable);
 }
