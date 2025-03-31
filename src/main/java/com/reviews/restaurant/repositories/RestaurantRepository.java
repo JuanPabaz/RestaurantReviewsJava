dfcbddf3f1b0a1947342354ef7d8d8c66910fef2
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-31 15:51:42 -0500

    feat: add find avg rating by restaurant id

--- src/main/java/com/reviews/restaurant/repositories/RestaurantRepository.java
+++ src/main/java/com/reviews/restaurant/repositories/RestaurantRepository.java
@@ -2,6 +2,12 @@ package com.reviews.restaurant.repositories;
 
 import com.reviews.restaurant.entities.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("SELECT AVG(re.totalScore) FROM Restaurant AS res INNER JOIN Review AS re " +
+            "ON res.idRestaurant = re.restaurant.idRestaurant WHERE res.idRestaurant = :idRestaurant")
+    Double findAvgRatingByRestaurant(@Param("idRestaurant") Long idRestaurant);
 }
