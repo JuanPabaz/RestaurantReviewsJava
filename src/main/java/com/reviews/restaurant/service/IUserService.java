91f3d13d21ec07ed4575a2b8da60f695f51a0e2e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-03 12:25:44 -0500

    feat: delete unused exception

--- src/main/java/com/reviews/restaurant/service/IUserService.java
+++ src/main/java/com/reviews/restaurant/service/IUserService.java
@@ -1,9 +1,8 @@
 package com.reviews.restaurant.service;
 
 import com.reviews.restaurant.entities.Favorites;
-import org.apache.coyote.BadRequestException;
 
 public interface IUserService {
 
-    Favorites addFavoriteRestaurant(Long restaurantId, Long userId) throws BadRequestException;
+    Favorites addFavoriteRestaurant(Long restaurantId, Long userId);
 }
