acd5ec49aed34e517b6eb3b75abca8c1f903126c
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-05 11:32:37 -0500

    feat: add has already restaurant as favorite validation and add getUserById method

--- src/main/java/com/reviews/restaurant/controllers/UserController.java
+++ src/main/java/com/reviews/restaurant/controllers/UserController.java
@@ -1,11 +1,9 @@
 package com.reviews.restaurant.controllers;
 
 import com.reviews.restaurant.dto.FavoriteResponseDTO;
+import com.reviews.restaurant.dto.UserResponseDTO;
 import com.reviews.restaurant.service.IUserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,7 +17,12 @@ public class UserController {
     }
 
     @PostMapping("/favorites")
-    public FavoriteResponseDTO addFavorites(@RequestParam Long idRestaurant, @RequestParam Long idUser) {
+    public FavoriteResponseDTO addFavorites(@RequestParam Long idRestaurant, @RequestParam Integer idUser) {
         return userService.addFavoriteRestaurant(idRestaurant,idUser);
+    }
+
+    @GetMapping("/{idUser}")
+    public UserResponseDTO getUserById(@PathVariable Integer idUser) {
+        return userService.getUserById(idUser);
     }
 }
--- src/main/java/com/reviews/restaurant/service/UserServiceImpl.java
+++ src/main/java/com/reviews/restaurant/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.reviews.restaurant.service;
 
 import com.reviews.restaurant.dto.FavoriteResponseDTO;
+import com.reviews.restaurant.dto.UserResponseDTO;
 import com.reviews.restaurant.entities.Favorites;
 import com.reviews.restaurant.entities.Restaurant;
 import com.reviews.restaurant.entities.User;
 import com.reviews.restaurant.exceptions.BadCreateRequest;
 import com.reviews.restaurant.maps.FavoriteMapper;
+import com.reviews.restaurant.maps.UserMapper;
 import com.reviews.restaurant.repositories.FavoriteRepository;
 import com.reviews.restaurant.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,7 @@ public class UserServiceImpl implements IUserService {
 
     private final FavoriteMapper favoriteMapper;
 
+
     public UserServiceImpl(UserRepository userRepository, IRestaurantService restaurantService, FavoriteRepository favoriteRepository, FavoriteMapper favoriteMapper) {
         this.userRepository = userRepository;
         this.restaurantService = restaurantService;
@@ -29,9 +32,17 @@ public class UserServiceImpl implements IUserService {
     }
 
     @Override
-    public FavoriteResponseDTO addFavoriteRestaurant(Long restaurantId, Long userId) {
+    public FavoriteResponseDTO addFavoriteRestaurant(Long restaurantId, Integer userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BadCreateRequest("Usuario no encontrado"));
+
+        boolean hasAlreadyAsFavorite = user.getFavoritesList()
+                .stream()
+                .anyMatch(favorite -> favorite.getRestaurant().getIdRestaurant().equals(restaurantId));
+
+        if (hasAlreadyAsFavorite) {
+            throw new BadCreateRequest("Ya tiene este restaurante como favorito");
+        }
 
         Restaurant restaurant = restaurantService.getRestaurantById(restaurantId)
                 .orElseThrow(() -> new BadCreateRequest("Restaurante no encontrado"));
@@ -41,5 +52,11 @@ public class UserServiceImpl implements IUserService {
         favorites.setUser(user);
 
         return favoriteMapper.mapFavorite(favoriteRepository.save(favorites));
+    }
+
+    @Override
+    public UserResponseDTO getUserById(Integer userId) {
+        return UserMapper.mapUsuario(userRepository.findById(userId)
+                .orElseThrow(() -> new BadCreateRequest("Usuario no encontrado")));
     }
 }
