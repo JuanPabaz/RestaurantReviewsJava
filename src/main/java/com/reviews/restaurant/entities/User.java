f336e96ecf078ad89b72b26287907a16e49c44c8
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-05 11:32:50 -0500

    feat: add image attribute to User

--- src/main/java/com/reviews/restaurant/entities/User.java
+++ src/main/java/com/reviews/restaurant/entities/User.java
@@ -33,6 +33,9 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "image")
+    private String image;
+
     @Enumerated(value = EnumType.STRING)
     private Role role;
 
