addf2e3431359a5bce878910bd50b01ecd9d1f08
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-05-02 16:40:57 -0500

    feat: add s3 url

--- src/main/java/com/reviews/restaurant/config/CorsConfig.java
+++ src/main/java/com/reviews/restaurant/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Agrega el origen de Angular
+        configuration.setAllowedOrigins(List.of("http://prueba-v2-angular.s3-website-us-east-1.amazonaws.com")); // Agrega el origen de Angular
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
